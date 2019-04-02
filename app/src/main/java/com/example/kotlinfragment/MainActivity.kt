08fd6f3d1fde3820645aa1a8b3206ae59db85976
roy <roychen11151002@gmail.com>
2019-04-02 10:27:16 +0800

    20190402

--- app/src/main/java/com/example/kotlinfragment/MainActivity.kt
+++ app/src/main/java/com/example/kotlinfragment/MainActivity.kt
@@ -2,11 +2,30 @@ package com.example.kotlinfragment
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentManager
+import android.support.v4.app.FragmentPagerAdapter
+import kotlinx.android.synthetic.main.activity_main.*
+
+const val Kotlin = "kotlinTest"
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val adapter = ViewPagerAdapter(supportFragmentManager)
+        viewPager.adapter = adapter
     }
+}
+
+class ViewPagerAdapter(fm: FragmentManager): FragmentPagerAdapter(fm) {
+    override fun getItem(p0: Int): Fragment = when(p0) {
+        0 -> FirstFragment()
+        1 -> SecondFragment()
+        else -> ThirdFragment()
+    }
+
+    override fun getCount(): Int = 3
 }
